cf48d3569b1871538453a1345c52faf4abc026c7
tiago <tiagockoch@gmail.com>
2026-03-08 18:01:45 -0300

    feat: Integrate OpenTelemetry for observability in Notification Worker
    - Add StandardDependencies.Injection and StandardDependencies.Models packages.
    - Configure OpenTelemetry in Program.cs and appsettings.json for tracing and metrics.
    - Update Microsoft.Extensions.Configuration package version in UnitTest.csproj.

--- src/Adapters/Driving/Notification.Worker/Program.cs
+++ src/Adapters/Driving/Notification.Worker/Program.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
 using final_challenge_grupo_118_notification;
 using final_challenge_grupo_118_notification.Models;
 using final_challenge_grupo_118_notification.Services;
 using final_challenge_grupo_118_notification.Services.Interface;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StandardDependencies.Injection;
+using StandardDependencies.Models;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -20,5 +24,16 @@ builder.Services.AddTransient<IEmailService, EmailService>();
 
 builder.Services.Configure<SmsSettings>(builder.Configuration.GetSection("SmsSettings"));
 builder.Services.AddTransient<ISmsService, SmsNotificationService>();
+
+var openTelemetryOptions = new OpenTelemetryOptions
+{
+    ServiceName = "NotificationWorker",
+    ServiceVersion = "1.0.0",
+    Url = "https://localhost:4318",
+    Exporters = new List<ExporterTypes> { ExporterTypes.OTLP }
+};
+
+builder.ConfigureCommonElements(openTelemetryOptions);
+
 var host = builder.Build();
 host.Run();
